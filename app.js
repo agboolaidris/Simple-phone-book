c4d9b3ce7230aeb174e30c29ecae0b68c1760711
idris agboola <agboolaisholaidreez@gmail.com>
2021-12-27 16:52:44 +0100

    update

--- app.js
+++ app.js
@@ -1,10 +1,24 @@
-function Mth() {
-  this.value = 700;
-  this.pop = () => {
-    return this.value;
-  };
+function PhoneBook() {
+  const books = [];
+
+  //@DESC this method is to validate phoneNumber
+  //@TYPE Private
+  function isPhoneNumber(inputtxt) {
+    const phoneno = /^\(?([0-9]{3})\)?[-. ]?([0-9]{3})[-. ]?([0-9]{4})$/;
+    if (inputtxt.value.match(phoneno)) {
+      return true;
+    } else {
+      return false;
+    }
+  }
+
+  //@DESC this method is to check if the phone number is exist
+  //@TYPE Private
+  function isExist(inputtxt) {
+    return books.indexOf(inputtxt) !== -1;
+  }
+
+  //@DESC this method is to add new phone number to the phone book
+  //@TYPE Public
+  
 }
-
-const pop = new Mth();
-
-console.log(pop.pop());
