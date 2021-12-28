e27ee5e71380ac4635169145b597cc9e4ca964e3
idris agboola <agboolaisholaidreez@gmail.com>
2021-12-28 08:29:44 +0100

    update

--- app.js
+++ app.js
@@ -1,5 +1,5 @@
 function PhoneBook() {
-  const books = [];
+  const database = [];
 
   //@DESC this method is to validate phoneNumber
   //@TYPE Private
@@ -18,25 +18,23 @@ function PhoneBook() {
       );
   }
 
-  //@DESC this method is to check if the phone number is exist
+  //@DESC this method is check unique phone numbers
   //@TYPE Private
-  function isExist(key, value) {
-    return books.findIndex((e) => e[key] == value) !== -1;
+  function isExist(value) {
+    return database.findIndex((e) => e.phoneNumber == value) !== -1;
   }
 
   //@DESC this method is to add new phone number to the phone book
   //@TYPE Public
-  function create(body) {
+  this.create = (body) => {
     const { phoneNumber, name, email } = body;
     return new Promise((resolve, reject) => {
       if (!isPhoneNumber(phoneNumber)) {
         reject("invalid phone number");
       } else if (!isEmail(email)) {
         reject("invalid email address");
-      } else if (isExist("phoneNumber", phoneNumber)) {
+      } else if (isExist(phoneNumber)) {
         reject("phone number already exist");
-      } else if (isExist("email", email)) {
-        reject("email already exist");
       } else if (!name || name.trim() == "") {
         reject("name is required");
       } else {
@@ -46,27 +44,39 @@ function PhoneBook() {
           name,
           created_at: Date.now(),
         };
-        books.push(new_book);
+        database.push(new_book);
         resolve(new_book);
       }
     });
-  }
+  };
 
+  //@DESC this method is to Get unique phone details using the phone number
+  //@TYPE Public
+  this.findByPhoneNumber = (phoneNumber) => {
+    const data = database.find((book) => book.phoneNumber == phoneNumber);
+    return new Promise((resolve, reject) => {
+      if (!data) reject("phone number doesn't not exist");
+      else resolve(data);
+    });
+  };
 
-  this.findByPhoneNumber=
-
-  return {
-    create,
+  //@DESC this method is Get list of phone numbers with details
+  //@TYPE Public
+  this.findAll = () => {
+    return new Promise((resolve, reject) => {
+      if (database.length < 1) reject("phone number doesn't not exist");
+      else resolve(database);
+    });
   };
 }
 
 const book = new PhoneBook();
 
-
+//create new
 book
   .create({ phoneNumber: "8137088555" })
   .then((res) => console.log(res))
-  .catch((err) => console.log("book-1", err));
+  .catch((err) => console.log(err));
 
 book
   .create({
@@ -74,5 +84,15 @@ book
     name: "adewale",
     email: "test@test.test",
   })
+  .then((res) => console.log(res))
+  .catch((err) => console.log(err));
+
+book
+  .findByPhoneNumber("8137088555")
+  .then((res) => console.log(res))
+  .catch((err) => console.log(err));
+
+book
+  .findAll()
   .then((res) => console.log(res))
   .catch((err) => console.log(err));
